607df3fee31b3c2c441c240c96ad12dc609ffc2f
Cuiks <1253016652@qq.com>
2021-05-13 17:33:32 +0800

    feat: operate system

---
+++ operate_system/queue.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+class ThreadSafeQueue(object):
+
+    # 当前队列元素数量
+    def size(self):
+        pass
+
+    # 往队列里放入元素
+    def put(self, item):
+        pass
+
+    def batch_put(self, item_list):
+        pass
+
+
+
